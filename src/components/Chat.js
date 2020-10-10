9b469bc943d942b2499576fbf051fc9be24ce72c
vincentmuriuki <vincent.muriuki@bskglobaltech.com>
2020-10-10 20:19:16 +0300

    Basic structure of discord

---
+++ src/components/Chat.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../styles/Chat.css';
+import ChatHeader from '../components/ChatHeader';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import { Avatar } from '@material-ui/core';
+import Message from './Message';
+
+function Chat() {
+  return (
+    <div className='chat'>
+      <ChatHeader />
+
+      <div className='chat__messages'>
+          <Message />
+      </div>
+
+      <div className='chat__input'>
+        <AddCircleIcon fontSize="large"/>
+        <form>
+            <input placeholder="Message #TESTCHANNEL"/>
+            <button className="chat__inputButton" type="submit">Send Message</button>
+        </form>
+        <div className="chat__inputIcons">
+            <CardGiftcardIcon fontSize="large"/>
+            <GifIcon fontSize="large"/>
+            <EmojiEmotionsIcon fontSize="large"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import '../styles/Message.css';
+
+function Message() {
+  return (
+    <div className="message">
+      <Avatar />
+    </div>
+  );
+}
+
+export default Message;
